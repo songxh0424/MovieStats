b08c1389ff7f80db0f20c6094f253fa0852fa8ae
songxh0424 <songxh@umich.edu>
2017-12-16 23:20:32 -0500

    added check all buttons

--- server.R
+++ server.R
@@ -1,1 +1,13 @@
-function(input, output, session) {}
+function(input, output, session) {
+  ## update the genre selection on stats page
+  observeEvent(input$checkAll, {
+    updateCheckboxGroupInput(session, label = NULL, inputId = 'genre',
+                             choices = genres, selected = genres)
+    }
+  )
+  observeEvent(input$uncheckAll, {
+    updateCheckboxGroupInput(session, label = NULL, inputId = 'genre',
+                             choices = genres, selected = NULL)
+    }
+  )
+}
