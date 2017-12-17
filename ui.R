c771139d7ab2cf5f5365a30f88ef44cc33d14386
songxh0424 <songxh@umich.edu>
2017-12-17 01:57:46 -0500

    added buttons to go to github repo and imdb

--- ui.R
+++ ui.R
@@ -7,7 +7,16 @@ fluidPage(
   fluidRow(
     dashboardPage(
       skin = 'purple',
-      header = dashboardHeader(title = 'MovieStats', titleWidth = 300),
+      header = dashboardHeader(title = 'MovieStats', titleWidth = 300,
+                               tags$li(a(href = 'https://www.imdb.com',
+                                         icon('imdb'), title = 'IMDb website',
+                                         style = 'font-size: 20px'),
+                                       class = 'dropdown'),
+                               tags$li(a(href = 'https://www.github.com/songxh0424/movies-dashboard',
+                                         icon('github'), title = 'Github repo',
+                                         style = 'font-size: 20px'),
+                                       class = 'dropdown')
+                               ),
       sidebar = dbSidebar,
       body = dbBody
     )
