efc468a57a9405e1b87b4c94055fb84da6bbd903
songxh0424 <songxh@umich.edu>
2017-12-19 22:23:20 -0500

    fixed a typo in side bar

--- sidebar.R
+++ sidebar.R
@@ -5,11 +5,11 @@ dbSidebar = dashboardSidebar(
     menuItem('Statistics and Rankings', tabName = 'stats', icon = icon('bar-chart')),
     menuItem('Directors', tabName = 'director', icon = icon('video-camera')),
     selectizeInput(inputId = 'search_director', label = NULL, choices = directors, selected = 'Quentin Tarantino',
-                   options = list(maxOptions = 5, placeholder = 'Input a director\' name')),
+                   options = list(maxOptions = 5, placeholder = 'Input a director\'s name')),
     menuItem('Actors', tabName = 'actor', icon = icon('user-secret')),
     ## too many actors seem to slow down the page significantly, maybe include only most popular ones
     selectizeInput(inputId = 'search_actor', label = NULL, choices = actors, selected = 'Uma Thurman',
-                   options = list(maxOptions = 5, placeholder = 'Input an actor\' name')),
+                   options = list(maxOptions = 5, placeholder = 'Input an actor\'s name')),
     tags$head(tags$script(HTML('$(document).ready(function() {$(".treeview-menu").css("display", "block");})')))
   )
 )
