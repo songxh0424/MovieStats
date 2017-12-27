08d6b6ad344d4e6947454ff0ee76fd2ee08356e0
songxh0424 <songxh@umich.edu>
2017-12-26 16:52:15 -0800

    use api to get actor name ids

--- getdata.R
+++ getdata.R
@@ -5,6 +5,7 @@ library(readr)
 library(httr)
 library(jsonlite)
 library(doParallel)
+library(parallel)
 
 cores = 24
 
@@ -61,11 +62,44 @@ movies.all = omdb %>% select(-Genre) %>%
 actors = lapply(1:nrow(omdb), function(i) {
   acts = str_split(omdb$Actors[i], ',')[[1]] %>% str_trim()
   data.frame(imdbID = rep(omdb$imdbID[i], length(acts)), Actor = acts)
-}) %>% bind_rows() %>% filter(Actor != NA & Actor != '')
+}) %>% bind_rows() %>% filter(!is.na(Actor) & Actor != '')
 ## directors table
 directors = lapply(1:nrow(omdb), function(i) {
   dirs = str_split(omdb$Director[i], ',')[[1]] %>% str_trim()
   data.frame(imdbID = rep(omdb$imdbID[i], length(dirs)), Director = dirs)
-}) %>% bind_rows() %>% filter(Director != NA & Director != '')
+}) %>% bind_rows() %>% filter(!is.na(Director) & Director != '')
 
 save(movies.all, actors, directors, file = 'movies.all.RData')
+load('movies.all.RData')
+
+## get the name ids
+## the api just mysterious broke, needs a new function
+get_nmid = function(name) {
+  ## url_format = 'http://www.imdb.com/xml/find?json=1&nr=1&nm=on&q=%s'
+  url_format = 'http://sg.media-imdb.com/suggests/%s/%s.json'
+  search_str = str_split(name, ' ')[[1]] %>% str_trim() %>% str_to_lower() %>%
+    paste0(collapse = '+')
+  url = sprintf(url_format, str_sub(search_str, end = 1), search_str)
+  response = GET(url) %>% content(as = 'text', encoding = 'utf-8')
+  idx = str_locate(response, '\\(')[1, 1]
+  response = fromJSON(str_sub(response, idx + 1, str_length(response) - 1))
+  ## nmid = response$name_popular$id
+  id = response$d$id[1]
+  image = response$d$i[[1]][1]
+  return(list(id = id, image = image))
+} 
+
+cores = 24 
+registerDoParallel(cores = cores)
+directorIDs = foreach(dir = unique(directors$Director)) %dopar%
+  tryCatch({print(dir); get_nmid(dir)},
+           error = function(e) {print(sprintf('Error: Director %s', dir)); return(NULL)}
+           )
+names(directorIDs) = unique(directors$Director)
+save(directorIDs, file = 'directorIDs.RData')
+actorIDs = foreach(act = unique(actors$Actor)) %dopar%
+  tryCatch(get_nmid(act),
+           error = function(e) {print(sprintf('Error: Actor %s', act)); return(act)}
+           )
+names(actorIDs) = unique(actors$Actor)
+save(actorIDs, file = 'actorIDs.RData')
